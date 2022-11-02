0179437cf12188c5cd7149449ce9abe9272c0932
Vilma Jalonen <corvuslintu@gmail.com>
2022-11-02 15:57:20 +0200

    hianosäätöö

--- Assets/Scripts/Vilma/UI_Handler.cs
+++ Assets/Scripts/Vilma/UI_Handler.cs
@@ -11,49 +11,44 @@ public class UI_Handler : MonoBehaviour
     [SerializeField] TMP_Text resourcesCollectedText;
     [SerializeField] Image launchPowerFillImage;
     [SerializeField] GameObject gameOverPanel;
-    LaunchTracker launchTracker;
-    Inventory inventory;
-    BallController controller;
+    
+    Helper helper;
 
-    private void Start()
+    void Start()
     {
-        launchTracker = FindObjectOfType<LaunchTracker>();
-        inventory = FindObjectOfType<Inventory>();
-        controller = FindObjectOfType<BallController>();
+        helper = FindObjectOfType<Helper>();
         UpdateLaunchesLeftAmount();
         UpdateKeysCollectedAmount();
         UpdateResourcesCollectedAmount();
     }
 
-    private void Update()
+    void Update()
     {
-        if (controller.ballSelected) UpdateSlider();
+        if (helper.controller.ballSelected) UpdateSlider();
     }
 
     void UpdateSlider()
     {
-        launchPowerFillImage.fillAmount =  controller.GetDistance() / controller.GetMaxDistance();
-        Debug.Log(controller.GetDistance());
-        Debug.Log(controller.GetMaxDistance());
+        launchPowerFillImage.fillAmount = helper.controller.GetDistance() / helper.controller.GetMaxDistance();
     }
 
     public void UpdateLaunchesLeftAmount()
     {
-        launchesLeftText.text = "Launches left: " + launchTracker.launchesLeft.ToString();
+        launchesLeftText.text = "Launches left: " + helper.launchTracker.launchesLeft.ToString();
     }
 
     public void UpdateKeysCollectedAmount()
     {
-        keysCollectedText.text = "Keys collected: " + inventory.KeysCollected;
+        keysCollectedText.text = "Keys collected: " + helper.inventory.KeysCollected;
     }
 
     public void UpdateResourcesCollectedAmount()
     {
-        resourcesCollectedText.text = "Resources collected: " + inventory.ResourcesCollected;
+        resourcesCollectedText.text = "Resources collected: " + helper.inventory.ResourcesCollected;
     }
 
     public void SetActiveGameOverPanel()
     {
-        if (launchTracker.launchesLeft <= 0) gameOverPanel.SetActive(true);
+        if (helper.launchTracker.launchesLeft <= 0) gameOverPanel.SetActive(true);
     }
 }
